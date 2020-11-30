f440e6a6d8159e26d99789dbabd724c2414242d9
uduver <oliverudu@gmail.com>
2020-11-30 22:19:42 +0200

    Add comments & organize styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// Kõik javascript siin failis on ise kirjutatud.
+
+// Kõik taustapildid, mida vahetatakse listina.
 const listOfBackgrounds = [
     "https://cdn.stocksnap.io/img-thumbs/960w/nature-snow_MYB1PRE1CG.jpg",
     "https://cdn.stocksnap.io/img-thumbs/960w/black%20&%20white-dog_XMC0W07QF8.jpg",
@@ -7,12 +10,16 @@ const listOfBackgrounds = [
 
 let currentBackground = 0;
 
+// Muudab iga 3 sekundi tagant veebilehel taustapilti.
 setInterval(() => {
     currentBackground += 1;
     currentBackground %= listOfBackgrounds.length
     document.body.style.backgroundImage = `url(${listOfBackgrounds[currentBackground]})`
 }, 3000)
 
+
+// Selle asemel, et inimene peaks koguaeg erinevat lehte külastama, fetchib ta lehe sisu ettenähtud failist.
+// Nii on kasutaja kogemus parem ja see hoiab ära koodi dubleerimist.
 function openLeht(leht) {
     fetch(leht).then((content) => {
         return content.text()
